5bc86dce07dcbf9f73661b43037b35419114effc
Shunya Kitada <syun.kitada@gmail.com>
2017-06-18 02:20:28 +0900

    update for python3

--- openstack-all/setup_common.sh
+++ openstack-all/setup_common.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt-get install -y apt-transport-https curl vim wget git libssl-dev nmap
-apt-get install -y python libpython-dev python-pip mysql-client nginx
+apt-get install -y python3 libpython3-dev python3-pip mysql-client nginx
 apt-get install -y ebtables iptables
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
@@ -12,7 +12,7 @@ apt-get install -y kubectl
 cd /tmp
 wget https://github.com/Yelp/dumb-init/releases/download/v1.2.0/dumb-init_1.2.0_amd64.deb
 dpkg -i dumb-init_*.deb
-pip2 install -r /tmp/system-requirements.txt
+pip3 install -r /tmp/system-requirements.txt
 
 # Install openstack-tools
 virtualenv /opt/openstack-tools
