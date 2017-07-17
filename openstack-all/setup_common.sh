cdc5e2ea8f7f67c0a7b37ec184dd9eeef4dd7243
Shunya Kitada <syun.kitada@gmail.com>
2017-07-17 22:47:16 +0900

    add centos7-rpm-builder

--- openstack-all/setup_common.sh
+++ openstack-all/setup_common.sh
@@ -1,7 +1,8 @@
 apt-get update
-apt-get install -y apt-transport-https curl vim wget git libssl-dev nmap
-apt-get install -y python3 libpython3-dev python3-pip mysql-client nginx
+apt-get install -y apt-transport-https curl vim wget git libssl-dev nmap traceroute
+apt-get install -y python libpython-dev python-pip mysql-client nginx
 apt-get install -y ebtables iptables
+
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
@@ -10,9 +11,13 @@ apt-get update
 apt-get install -y kubectl
 
 cd /tmp
+wget https://storage.googleapis.com/kubernetes-helm/helm-v2.4.2-linux-amd64.tar.gz
+tar -xf helm-v2.4.2-linux-amd64.tar.gz
+mv linux-amd64/helm /usr/local/bin/
+
 wget https://github.com/Yelp/dumb-init/releases/download/v1.2.0/dumb-init_1.2.0_amd64.deb
 dpkg -i dumb-init_*.deb
-pip3 install -r /tmp/system-requirements.txt
+pip install -r /tmp/system-requirements.txt
 
 # Install openstack-tools
 virtualenv /opt/openstack-tools
