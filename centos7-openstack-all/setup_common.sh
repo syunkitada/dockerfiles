2ee91e5e0ba3144557fc7b72745ab4069e38e18c
Shunya Kitada <syun.kitada@gmail.com>
2017-07-30 21:10:14 +0900

    update openstack

--- centos7-openstack-all/setup_common.sh
+++ centos7-openstack-all/setup_common.sh
@@ -4,9 +4,10 @@ yum install -y mariadb
 yum install -y nginx
 yum install -y openstack-tools nova keystone neutron glance horizon cinder heat
 yum install -y kubectl
+yum install -y ebtables sysfsutils dbus genisoimage vde2-2.3.2 qemu-2.9.0
 
 cd /tmp
-wget https://storage.googleapis.com/kubernetes-helm/helm-v2.4.2-linux-amd64.tar.gz
-tar -xf helm-v2.4.2-linux-amd64.tar.gz
-mv linux-amd64/helm /usr/local/bin/
+wget https://storage.googleapis.com/kubernetes-helm/helm-v2.5.1-linux-amd64.tar.gz && \
+tar -xf helm-v2.5.1-linux-amd64.tar.gz && \
+mv linux-amd64/helm /usr/local/bin/ && \
 rm -rf /tmp/*
