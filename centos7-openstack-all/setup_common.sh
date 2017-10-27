83f14acb4224d37ca0445aef3cd1979d9db1b4ee
Shunya Kitada <syun.kitada@gmail.com>
2017-10-27 23:50:27 +0900

    update

--- centos7-openstack-all/setup_common.sh
+++ centos7-openstack-all/setup_common.sh
@@ -7,7 +7,7 @@ yum install -y kubectl
 yum install -y ebtables bridge-utils sysfsutils dbus genisoimage vde2-2.3.2 qemu-2.9.0
 
 cd /tmp
-wget https://storage.googleapis.com/kubernetes-helm/helm-v2.5.1-linux-amd64.tar.gz && \
-tar -xf helm-v2.5.1-linux-amd64.tar.gz && \
+wget https://storage.googleapis.com/kubernetes-helm/helm-v2.6.1-linux-amd64.tar.gz && \
+tar -xf helm-v2.6.1-linux-amd64.tar.gz && \
 mv linux-amd64/helm /usr/local/bin/ && \
 rm -rf /tmp/*
